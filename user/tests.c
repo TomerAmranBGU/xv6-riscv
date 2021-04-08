cd2a172d7179d8742aed22034d34350a831822fb
tomeramranBGU-pc <amrant@post.bgu.ac.il>
2021-04-08 14:08:17 +0300

    tests for 4.4

--- user/tests.c
+++ user/tests.c
@@ -9,7 +9,7 @@ main(int argc, char *argv[]){
     if (pid ==0){    
         // fprintf(2,"parent set priority and: %d\n",set_priority(LOW));
         int j =0;
-        for (int i =0; i<10000000; i++){
+        for (int i =0; i<10000; i++){
             j++;
             if (j%100 ==0){
                 fprintf(2,"child arrived to %d\n",j);
@@ -19,10 +19,10 @@ main(int argc, char *argv[]){
     else {
         fprintf(2,"child set priority and: %d\n",set_priority(HIGH));  
         int j =0;
-        for (int i =0; i<10000000; i++){
+        for (int i =0; i<10000; i++){
             j++;
             if (j%100 ==0){
-                fprintf(2,"child arrived to %d\n",j);
+                fprintf(2,"parent arrived to %d\n",j);
             }
         }
     }
